1c9b54b1c8b5ed26ad5a715d98cc792623931392
the-unknown-code <hello@unk.digital>
2026-02-23 17:16:24 +0700

    hero updaye

--- app/components/home/Hero.vue
+++ app/components/home/Hero.vue
@@ -1,13 +1,16 @@
-<script setup lang="ts"></script>
-
 <template>
   <section class="home-hero">
-    <div class="home-hero__bg">
-      <div v-for="i in 8"
-        :key="i"
-        class="home-hero__bg--item"></div>
-    </div>
-    <div class="home-hero__inner"
+    <client-only>
+      <div class="home-hero__bg">
+        <div v-for="i in 8"
+          :key="i"
+          class="home-hero__bg--item">
+          <div />
+        </div>
+      </div>
+    </client-only>
+    <div v-show="isDesktop"
+      class="home-hero__inner"
       role="h1"
       aria-label="Everything start with one Input">
       <div class="row row-1">
@@ -41,8 +44,20 @@
         </div>
       </div>
     </div>
+    <div v-show="!isDesktop"
+      class="home-hero__mobile">
+      <h1>Everything <b>starts</b> with one <b>Input</b></h1>
+      <div class="media">
+        <div></div>
+      </div>
+    </div>
   </section>
 </template>
+
+<script setup lang="ts">
+const { isDesktop } = useBreakpoints();
+</script>
+
 
 <style lang="scss" scoped>
 .home-hero {
@@ -58,26 +73,46 @@
     @include fill(absolute);
     width: 100%;
     display: flex;
-    flex-direction: row-reverse;
+    flex-direction: row;
     justify-content: space-between;
 
     &::after {
       content: '';
       @include fill(absolute);
-      background: linear-gradient(135deg, var(--darkgrey) 20%, transparent 160%);
+      background: linear-gradient(135deg, var(--darkgrey) 30%, transparent 160%);
     }
 
     &--item {
       position: relative;
-      background: linear-gradient(180deg, var(--darkgrey) 20%, var(--theme-contrast) 100%);
+
+      width: clamp(100%, 100% / 8, 100%);
 
       @for $i from 1 through 8 {
         &:nth-child(#{$i}) {
-          width: calc(1.5% * #{$i - 1});
+          >div {
+            position: absolute;
+            left: 0;
+            width: calc(90% / 8 * #{$i});
+            height: 100%;
+            background: linear-gradient(180deg, var(--darkgrey) 0%, var(--theme-contrast) 100%);
+          }
+
         }
       }
+
     }
 
+  }
+
+  &__mobile {
+    position: relative;
+    text-wrap: balance;
+    padding-bottom: var(--spacer-32);
+
+    h1 {
+      line-height: 1.25cap;
+      text-align: center;
+    }
   }
 
   &__inner {
@@ -85,6 +120,7 @@
     height: 100%;
     display: flex;
     flex-direction: column;
+    padding: var(--spacer-64) 0;
     gap: var(--spacer-32);
 
     .row {
@@ -119,24 +155,31 @@
       }
     }
 
-    .media {
-      position: relative;
-      display: flex;
-      align-self: stretch;
-      align-items: center;
-      width: 100%;
-      height: auto;
+  }
 
-      &:not(.empty) {
-        >div {
-          @include fill(relative);
-          width: 100%;
-          height: 100%;
-          background-color: var(--theme-contrast);
-          border-radius: var(--spacer-16);
-        }
+  .media {
+    position: relative;
+    display: flex;
+    align-self: stretch;
+    align-items: center;
+    width: 100%;
+    height: auto;
+
+    @include mobile {
+      margin-inline: auto;
+      max-width: 360px;
+      aspect-ratio: 4;
+      margin-top: var(--spacer-16);
+    }
+
+    &:not(.empty) {
+      >div {
+        @include fill(relative);
+        width: 100%;
+        height: 100%;
+        background-color: var(--theme-contrast);
+        border-radius: var(--spacer-16);
       }
-
     }
 
   }
